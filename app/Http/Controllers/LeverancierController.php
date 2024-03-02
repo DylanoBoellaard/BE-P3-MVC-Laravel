1bc385c628836af45f8d514a4316ec3e79c09953
DylanoBoellaard <336751@student.mboutrecht.nl>
2024-03-02 14:44:39 +0100

    Actually only now finished Scenario01 - US1

--- app/Http/Controllers/LeverancierController.php
+++ app/Http/Controllers/LeverancierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Leverancier;
+use App\Models\ProductsPerLeverancier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +11,7 @@ class LeverancierController extends Controller
 {
     public function index()
     {
-        // Get product details
+        // Get leverancier details
         $leverancierList = Leverancier::select(
             'leveranciers.id',
             'leveranciers.naam',
@@ -31,9 +32,22 @@ class LeverancierController extends Controller
 
     public function geleverdeProducten($leverancierId)
     {
+        // Get leverancier info
+        $leverancierInfo = Leverancier::find($leverancierId);
+
+        // Get leverancier geleverde producten details
+        $leveringList = DB::table('products')
+            ->select('products.id', 'products.naam', 'magazijns.aantalAanwezig', 'magazijns.verpakkingsEenheid', DB::raw('MAX(productsPerLeveranciers.datumLevering) as datumLevering'))
+            ->join('magazijns', 'products.id', '=', 'magazijns.productsId')
+            ->join('productsPerLeveranciers', 'products.id', '=', 'productsPerLeveranciers.productsId')
+            ->where('productsPerLeveranciers.leveranciersId', $leverancierId)
+            ->orderBy('magazijns.aantalAanwezig', 'desc')
+            ->groupBy('products.id', 'products.naam', 'magazijns.aantalAanwezig', 'magazijns.verpakkingsEenheid')
+            ->get();
 
         return view('leverancier.geleverdeProducten', [
-            'leverancierId' => $leverancierId // change
+            'leverancierInfo' => $leverancierInfo,
+            'leveringList' => $leveringList
         ]);
     }
 }
--- resources/views/leverancier/geleverdeProducten.blade.php
+++ resources/views/leverancier/geleverdeProducten.blade.php
@@ -4,8 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Geleverde Producten</title>
+    @vite(['resources/scss/magazijn/index.scss', 'resources/scss/magazijn/global.scss'])
 </head>
 <body>
-    <h1>geleverdeProducten</h1>
+    <div class="container">
+        <h1>Geleverde Producten</h1>
+        <!-- Display leverancier info -->
+        <div id="productInfo">
+            <p><span>Naam leverancier:</span> {{$leverancierInfo->naam}}</p>
+            <p><span>Contactpersoon:</span> {{$leverancierInfo->contactPersoon}}</p>
+            <p><span>Leverancier NR:</span> {{$leverancierInfo->leverancierNummer}}</p>
+            <p><span>Mobiel:</span> {{$leverancierInfo->mobiel}}</p>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Naam product</th>
+                    <th>Aantal in magazijn</th>
+                    <th>Verpakkingseenheid</th>
+                    <th>Laatste levering</th>
+                    <th>Nieuwe levering</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($leveringList as $levering) <!-- Foreach Loop to display all product details -->
+                <tr>
+                    <td>{{$levering->naam}}</td>
+                    <td>{{$levering->aantalAanwezig}}</td>
+                    <td>{{$levering->verpakkingsEenheid}}</td>
+                    <td>{{$levering->datumLevering}}</td>
+                    <td>
+                        <a href="{{route('leverancier.geleverdeProducten', [$levering -> naam])}}">
+                            <img class="small-img" src="/img/Box.png" alt="Box.png">
+                        </a>
+                    </td>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
